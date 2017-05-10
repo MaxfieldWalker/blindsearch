38ff93563cf77c39aafff3727a37377beaba47ca
Maxfield Walker <maxfield.walker@gmail.com>
2017-05-10 16:10:39 +0900

    tree structure implementation

--- src/main.rs
+++ src/main.rs
@@ -6,32 +6,129 @@ use std::io::prelude::*;
 
 use regex::Regex;
 
+use std::collections::HashMap;
+use std::collections::hash_map::Entry;
+
 mod tree;
 
-fn main() {
-    let input_path = "./tests/example.txt";
-    let file = File::open(input_path).unwrap();
-    let reader = BufReader::new(file);
+use tree;
 
-    for l in reader.lines() {
-        let line = l.unwrap();
-        // 正規表現を作る
-        let pattern = Regex::new(r"([a-zA-Z])\s+->\s+(\*?[a-zA-Z])+").unwrap();
-        // lineは 'String' だが， '&' をつけると '&str' になる
-        let captures = pattern.captures(&line).unwrap();
-        // 1つめのキャプチャを取得
-        let first = captures.get(1).unwrap().as_str();
-        // 表示
-        println!("{}", first);
+// 間違いないだろう
+fn capture<'a>(s: &'a String) -> regex::Captures<'a> {
+    let pattern = Regex::new(r"([a-zA-Z])\s+->\s+((\*?[a-zA-Z])+)").unwrap();
+    pattern.captures(s).unwrap()
+}
+
+// 1行を与えて、その節点が目標節点かどうかと節点名を返す
+fn getInfo<'a>(line: &'a String) -> (String, Vec<(bool, String)>) {
+    let captures = capture(line);
+    let first = captures.get(1).unwrap().as_str();
+    let mut children: Vec<(bool, String)> = vec![];
+
+    let rest = captures.get(2).unwrap().as_str();
+
+
+    let mut index = 0;
+    while index < rest.len() {
+        // 一文字だけ得る
+        let sub = &rest[index..index + 1];
+        let mut node_name: &str = sub;
+        let mut is_goal_node = false;
+        index += 1;
+
+        // '*'がついている、つまり目標節点の場合は
+        // さらにポインタを1すすめる
+        if sub == "*" {
+            // 部分文字列を取得している
+            node_name = &rest[index..index + 1];
+            index += 1;
+            is_goal_node = true;
+        }
+
+        children.push((is_goal_node, node_name.to_string()));
     }
 
-    let mut root_node = tree::Node::new("A");
+    (first.to_string(), children)
+}
 
-    let c_node = tree::Node::new("c");
-    root_node.add_child(c_node);
+fn f(line: String, root_node: &mut tree::Node, goals: &mut Vec<String>) {
+    let (node_name, children) = getInfo(&line);
 
-    let d_node = tree::Node::new("d");
-    root_node.add_child(d_node);
+    let samekey: String = node_name.clone();
 
-    root_node.stringify();
+    // 仮の親ノード
+    // &mut参照する可能性があるのでmutをつける必要がある
+    let mut node = tree::Node::new(samekey.to_string());
+
+
+    // 間違い無し
+    let mut parent_node: &mut tree::Node = match root_node.findNode(&node_name) {
+        Some(n) => {
+
+            // let a: () = n;
+            n
+        }
+        None => {
+            let n: &mut tree::Node = &mut node;
+            n
+        }
+    };
+
+
+    // ルートノードを取得する方法はまた考えよう
+    // ひとまずディクショナリからキーを検索する方法で回避する
+
+    for (isGoalNode, name) in children {
+
+        let samekey: String = name.clone();
+        let child_node = tree::Node::new(samekey.to_string());
+
+        // 親ノードに子ノードを追加する
+        parent_node.add_child(child_node);
+
+        if isGoalNode {
+            // ここでnameの所有権が移る
+            goals.push(name);
+        }
+
+    }
+}
+
+fn main() {
+    let root_node = TreeNode::new("Hello".to_string());
+
+    let a_node = TreeNode::new("a".to_string());
+
+    let b_node = TreeNode::new("b".to_string());
+
+    root_node.add_child(&a_node);
+    root_node.add_child(&b_node);
+
+    // root_nodeの型は 'Node<String>'
+    let a: () = root_node;
+
+
+    // let input_path = "./tests/example.txt";
+    // let file = File::open(input_path).unwrap();
+    // let mut reader = BufReader::new(file);
+
+    // // 目標節点を格納する配列
+    // let mut goals: Vec<String> = vec![];
+
+    // // 1行目を読む
+    // let mut first_line = String::new();
+    // reader.read_line(&mut first_line);
+
+    // // パースする
+    // let (node_name, _) = getInfo(&first_line);
+
+    // // ルートノードを作成する
+    // let mut root_node = tree::Node::new(node_name);
+
+    // f(first_line, &mut root_node, &mut goals);
+
+    // for l in reader.lines() {
+    //     let line = l.unwrap();
+    //     f(line, &mut root_node, &mut goals);
+    // }
 }
--- src/tree.rs
+++ src/tree.rs
@@ -1,61 +1,98 @@
-pub struct Node<'a> {
-    pub name: &'a str,
-    children: Vec<Box<Node<'a>>>,
+use std::rc::Rc;
+use std::cell::RefCell;
+
+type NodeRef = Rc<RefCell<_Node>>;
+
+#[derive(Debug)]
+struct _Node {
+    value: String,
+    children: Vec<NodeRef>,
 }
 
-impl<'a> Node<'a> {
-    pub fn new(n: &'a str) -> Node<'a> {
-        Node {
-            name: n,
+// NodeRefは単なるタイプエイリアスなので
+// タプル構造体を作ってまったく違う型にしてしまう。
+struct Node(NodeRef);
+
+impl Node {
+    fn new(value: String) -> Node {
+        let node = _Node {
+            value: value,
             children: vec![],
-        }
+        };
+        Node(Rc::new(RefCell::new(node)))
     }
 
-    pub fn add_child4(&mut self, name: &'a str) {
-        let new_node = Node::new(name);
-        self.children.push(Box::new(new_node));
+    pub fn add_child(&self, child_node: &Node) {
+        // ミュータブルな参照を得る
+        // 'self.0' はタプル構造体の1つめの要素を取得している
+        // つまり 'self.0' の型は 'Rc<RefCell<Node<T>>>' だが、
+        // Rc<T>は '<Target=T>' を実装しているので自動的に
+        // 'self.0' は 'RefCell<Node<T>>' 型に型強制される。
+        // RefCell<T>の 'borrow_mut' 関数はラップしている値の
+        // ミュータブルな参照を得る関数である。
+        let mut me = self.0.borrow_mut();
+
+        // 子ノードへの参照を得る
+        // cloneしているが、子ノードへのポインタをクローンしているので
+        // 値の複製が起こっているわけではない
+        let child_node_ref = child_node.0.clone();
+        me.children.push(child_node_ref);
     }
 
-    pub fn add_child2(&mut self, node: Box<Node<'a>>) {
-        self.children.push(node);
-    }
-
-    pub fn add_child(&mut self, node: Node<'a>) {
-        self.children.push(Box::new(node));
-    }
-
-    // - 自身の内容
-    //     - 子供
-    //         - さらに子供
-    //     - 子供
-    //     - 子供
     pub fn stringify(&self) {
+        // 自分の内容を表示
         self.inner_stringify(0);
     }
 
-    fn inner_stringify(&self, depth: u32) {
-        // depthの数だけスペースを生成している
-        let spaces = (0..depth).map(|_| " ").collect::<String>();
-        println!("{}- {}", spaces, self.name);
-        if !self.children.is_empty() {
-            // ここでも借用する必要がある
-            let children = &self.children;
-            // childはBox<Node<'a>>なのでrefキーワードで参照する必要がある
-            for ref child in children {
-                child.inner_stringify(depth + 4);
+    fn inner_stringify(&self, depth: i32) {
+        // depthの数だけスペースを生成する
+        let spaces = (0..depth * 4).map(|_| " ").collect::<String>();
+
+        let node_ref = self.0.borrow();
+
+        println!("{}- {}", spaces, node_ref.value);
+        if !node_ref.children.is_empty() {
+            for child_ref in &node_ref.children {
+                Node(child_ref.clone()).inner_stringify(depth + 1);
             }
+        }
+    }
+
+    fn find_node(&self, value: &str) -> Option<Node> {
+        let node_ref = self.0.borrow();
+        if node_ref.value == value {
+            return Some(Node(self.0.clone()));
+        } else {
+            for child_ref in &node_ref.children {
+                let child_node = Node(child_ref.clone());
+                match child_node.find_node(&value) {
+                    Some(node) => {
+                        return Some(node);
+                    }
+                    None => {
+                        // 何もしない
+                    }
+                }
+            }
+
+            return None;
         }
     }
 }
 
-// enum: 列挙型とは言うが
-// Union Type(直和型)に近い e.g. str | Nil
-pub enum Tree {
-    // 自身の内容と次の要素へのポインタを持つ
-    // タプル構造体である(Pairという名前を付けているが何でもいい)
-    Node,
+// fn main() {
+//     let root_node = Node::new("S".to_string());
 
-    // 空であることを表す
-    // フィールドを持たない 'Unit-like struct' である
-    Nil,
-}
+//     let a_node = Node::new("a".to_string());
+//     let b_node = Node::new("b".to_string());
+
+//     root_node.add_child(&a_node);
+//     root_node.add_child(&b_node);
+
+//     root_node.stringify();
+
+//     match root_node.find_node("d") {
+//         Some(node) => println!("FOUND"),
+//         None => println!("NOT FOUND"),
+//     }
+// }
